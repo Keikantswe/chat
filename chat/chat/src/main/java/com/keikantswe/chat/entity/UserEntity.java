409f93f98eb2d93a2fb3d96398d10f316a319515
Keikantswe More <113474880+Keikantswe@users.noreply.github.com>
2023-12-07 11:27:47 +0200

    Create UserEntity.java

---
+++ chat/chat/src/main/java/com/keikantswe/chat/entity/UserEntity.java
@@ -0,0 +1,18 @@
+package com.keikantswe.chat.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+}
