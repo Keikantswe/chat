46e48f36e563d5867d624b25689d504473057478
Keikantswe More <mkeikantswe@gmail.com>
2023-12-09 16:14:41 +0200

    Worked on my components and designs

--- chat-ui/src/components/Chat.js
+++ chat-ui/src/components/Chat.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import more from "../images/pngwing.com.png"
+import Messages from "./Messages"
+import InputMessage from "./InputMessage"
 
 const Chat = () => {
   return (
     <div className='Chat'>
-      <div>
+      <div className='ChatInfo'>
         <span> Kelly </span> 
+        <img src={more} alt=''/>
       </div>  
+
+      <Messages/>
+      <Messages/>
+      <Messages/>
+      <Messages/>
+      <Messages/>
+      <InputMessage/>
     </div>
   )
 }
--- chat-ui/src/components/InputMessage.js
+++ chat-ui/src/components/InputMessage.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
+
 const InputMessage = () => {
   return (
-    <div>InputMessage</div>
+    <div className='InputMessage'>
+      <input type='text' placeholder='Type message...'/>
+      <img src='' alt=''/>
+      <button>Send</button>
+    </div>
   )
 }
 
