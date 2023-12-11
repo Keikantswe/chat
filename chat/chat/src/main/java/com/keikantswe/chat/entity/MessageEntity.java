411745e1dd576f36c60608210a274ab1975a22b3
Keikantswe More <113474880+Keikantswe@users.noreply.github.com>
2023-12-11 14:09:58 +0200

    Update MessageEntity.java

--- chat/chat/src/main/java/com/keikantswe/chat/entity/MessageEntity.java
+++ chat/chat/src/main/java/com/keikantswe/chat/entity/MessageEntity.java
@@ -8,26 +8,24 @@ import java.util.Date;
 
 @Entity
 @Data
-@Table(name = "messages")
+@Table(name = "message")
 public class MessageEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private String messageId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long messageId;
 
     @Lob
     private String message;
     private String file;
     private Date timeStamp;
 
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private UserEntity sender;
 
-     @ManyToOne(
-     cascade = CascadeType.ALL
-     )
-     @JoinColumn(
-     name  = "User_Id",
-     referencedColumnName = "id"
-     )
-     private UserEntity userEntity;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private UserEntity receiver;
 
 }
