55e0040399b1635f782b952a0ff0636f8d6b5f07
Keikantswe More <113474880+Keikantswe@users.noreply.github.com>
2023-12-12 11:48:23 +0200

    navigating pages
    Added Routes to navigate easily through my application

--- chat-ui/src/App.js
+++ chat-ui/src/App.js
@@ -3,10 +3,18 @@ import "./style.scss"
 import Registration from "./pages/Registration"
 import Login from './pages/Login';
 import Home from './pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Home/>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login/>}></Route>
+        <Route path='/register' element={<Registration/>}></Route>
+        <Route path='/chat' element={<Home/>}></Route>
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
