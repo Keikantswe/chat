39482302f934f69d9fa0eebbe70355ca476ed479
Keikantswe More <113474880+Keikantswe@users.noreply.github.com>
2024-01-31 11:05:33 +0200

    Documentation

--- chat/chat/src/main/java/com/keikantswe/chat/controller/ContactController.java
+++ chat/chat/src/main/java/com/keikantswe/chat/controller/ContactController.java
@@ -1,7 +1,9 @@
 package com.keikantswe.chat.controller;
 
 import com.keikantswe.chat.entity.ContactEntity;
+import com.keikantswe.chat.entity.UserEntity;
 import com.keikantswe.chat.service.ContactService;
+import com.keikantswe.chat.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,12 +21,20 @@ public class ContactController {
     @Autowired
     private ContactService contactService;
 
+    @Autowired
+    private UserService userService;
+
+
+    private List<UserEntity> userId;
+
     @GetMapping("/friend/{userName}")
     public ResponseEntity<List<ContactEntity>> searchFriend(@PathVariable("userName") String userName){
 
         //fix this code
         //Github in Intellij
-        List<ContactEntity> searchedFriend = contactService.searchFriend(userName);
+        //List<ContactEntity> searchedFriend = contactService.searchFriend(userName);
+
+        userId = userService.searchFriend(userName);
 
         if (searchedFriend != null){
             return ResponseEntity.ok(searchedFriend);
